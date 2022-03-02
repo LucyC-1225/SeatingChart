38d8d63909b0f96f9721b190412e5c1b9c514e3e
LucyC-1225 <90148310+LucyC-1225@users.noreply.github.com>
2022-03-01 22:26:34 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         String[] seats = {"Moiz", "Angie", "Taran", "Kelvin", "Kyler", "David", "WenHao", "Nicole", "Jennifer", "Michelle", "Beckett", "Raymond", "Lucy", "Apramjot", "Justin Lema", "Sam", "Tristan", "Pradeep", "Mohammad", "Haley", "empty", "Rely", "Cheng Han", "Qihan", "Kevin", "Ryan", "Justin Liu", "Jeffrey", "Danny", "Elliot", "Benson", "Fiona", "Neil", "Kaitlyn"};
